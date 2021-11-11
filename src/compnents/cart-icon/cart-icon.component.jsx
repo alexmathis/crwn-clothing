651b3d29240d5fe24f580647ccbbdd502d730b9c
alexmathis <alex.mathis.sirknight@gmail.com>
2021-11-11 10:25:03 -0500

    added shopping cart

--- src/compnents/cart-icon/cart-icon.component.jsx
+++ src/compnents/cart-icon/cart-icon.component.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg";
+import {connect} from 'react-redux';
+import {toggleCartHidden} from '../../redux/cart/car.actions';
 
-const CartIcon = () => (
+const CartIcon = ({toggleCartHidden}) => (
 
-    <div className='cart-icon'>
+    <div className='cart-icon' onClick={toggleCartHidden}>
      <ShoppingIcon className='shopping-icon'/>
      <span className='item-count'>0</span>
     </div>
 )
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default connect(
+    null,
+    mapDispatchToProps)
+    (CartIcon);
