54b4e67374c129a59d911c749ec33f01157eb6a1
alexmathis <alex.mathis.sirknight@gmail.com>
2021-11-16 12:29:24 -0500

    set up redux for collections

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import CollectionPreview from '../../compnents/collection-preview/collection-preview-component.jsx';
-import  SHOP_DATA from './shop.data.js'
+import CollectionsOverview from '../../compnents/collections-overview/collections-overview.component'; 
+import { Route } from 'react-router';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
+const ShopPage = ({match}) => (
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:categoryId`} component={CollectionPage}/>
+    </div>
+);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return (<div className="shop-page">
-        {
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key ={id} {...otherCollectionProps}/>
-            ))
-        }
-        </div>);
-    }
-}
-
-export default ShopPage;
+export default (ShopPage);
