ff88345deaf8af1ef5ab3ecda0d31913b76942f8
Alexander Mathis <alex.mathis.sirknight@gmail.com>
2021-12-14 09:51:40 -0500

    Added media queries for mobile responsiveness.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import { GlobalStyle } from "./global.styles";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
@@ -20,6 +20,7 @@ const App = () => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
